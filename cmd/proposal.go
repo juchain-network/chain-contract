31a20c7ae6a9543cd96af38b894dfdea47513fed
Emmett <litian33@gmail.com>
2025-03-28 18:33:59 +0800

    增加系统合约管理命令

---
+++ cmd/proposal.go
@@ -0,0 +1,127 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/spf13/cobra"
+	"juchain.org/chain/contract/contracts/generated/proposal"
+)
+
+const (
+	validatorAddr = "0x000000000000000000000000000000000000f000"
+	punishAddr    = "0x000000000000000000000000000000000000f001"
+	proposalAddr  = "0x000000000000000000000000000000000000f002"
+)
+
+func CreateProposalCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create_proposal",
+		Short: "create proposal tx",
+		Run:   createProposalTx,
+	}
+	createProposalFlags(cmd)
+	return cmd
+}
+
+func createProposalFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP("proposer", "p", "", "proposer addr (must be valid validator)")
+	_ = cmd.MarkFlagRequired("proposer")
+	cmd.Flags().StringP("target", "t", "", "target addr (to be a validator)")
+	_ = cmd.MarkFlagRequired("target")
+	cmd.Flags().StringP("operation", "o", "", "operation add|remove")
+	_ = cmd.MarkFlagRequired("operation")
+}
+
+func createProposalTx(cmd *cobra.Command, _ []string) {
+	rpc, _ := cmd.Flags().GetString("rpc_laddr")
+	proposer, _ := cmd.Flags().GetString("proposer")
+	target, _ := cmd.Flags().GetString("target")
+	operation, _ := cmd.Flags().GetString("operation")
+	flag := true
+	if operation == "add" {
+		flag = true
+	} else if operation == "remove" {
+		flag = false
+	} else {
+		fmt.Printf("Invalid operation %s\n", operation)
+		return
+	}
+
+	innerCreateProposal(proposer, target, flag, rpc)
+}
+
+func innerCreateProposal(proposer, target string, flag bool, rpc string) {
+	proposalAbi, err := abi.JSON(strings.NewReader(proposal.ProposalABI))
+	if err != nil {
+		fmt.Println("JSON NewReader Err:", err)
+		return
+	}
+
+	abiData, err := proposalAbi.Pack("createProposal", common.HexToAddress(target), flag, "")
+	if err != nil {
+		fmt.Println("proposalAbi.Pack createProposal Err:", err)
+		return
+	}
+	err = CreateRawTx(common.HexToAddress(proposer), common.HexToAddress(proposalAddr), nil, abiData, rpc, "createProposal.json")
+	if err != nil {
+		fmt.Println("create tx Err:", err)
+		return
+	}
+	fmt.Println("crete tx success!")
+}
+
+func VoteProposalCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "vote_proposal",
+		Short: "vote proposal tx",
+		Run:   voteProposalTx,
+	}
+	voteProposalCmdFlags(cmd)
+	return cmd
+}
+
+func voteProposalCmdFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP("signer", "s", "", "singer addr (must be valid validator)")
+	_ = cmd.MarkFlagRequired("signer")
+	cmd.Flags().StringP("proposalId", "i", "", "proposal id")
+	_ = cmd.MarkFlagRequired("proposalId")
+	cmd.Flags().BoolP("approve", "a", false, "approve this proposal or not")
+	_ = cmd.MarkFlagRequired("approve")
+}
+
+func voteProposalTx(cmd *cobra.Command, _ []string) {
+	rpc, _ := cmd.Flags().GetString("rpc_laddr")
+
+	signer, _ := cmd.Flags().GetString("signer")
+	proposalId, _ := cmd.Flags().GetString("proposalId")
+	approve, _ := cmd.Flags().GetBool("approve")
+
+	innerVoteProposal(signer, proposalId, approve, rpc)
+}
+
+func innerVoteProposal(signer, proposalId string, flag bool, rpc string) {
+
+	proposalAbi, err := abi.JSON(strings.NewReader(proposal.ProposalABI))
+	if err != nil {
+		fmt.Println("JSON NewReader Err:", err)
+		return
+	}
+
+	var proposalIdBytes [32]byte
+	copy(proposalIdBytes[:], common.HexToHash(proposalId).Bytes())
+
+	abiData, err := proposalAbi.Pack("voteProposal", proposalIdBytes, flag)
+	if err != nil {
+		fmt.Println("proposalAbi.Pack createProposal Err:", err)
+		return
+	}
+	err = CreateRawTx(common.HexToAddress(signer), common.HexToAddress(proposalAddr), nil, abiData, rpc, "voteProposal.json")
+	if err != nil {
+		fmt.Printf("create tx error:%v", err)
+		return
+	}
+	fmt.Println("create tx success!")
+}
