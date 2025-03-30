efa7b13903e880e7a6f4a2d027d66f3ac791536d
hezhengjun <hezhengjun518@gmail.com>
2025-03-30 21:16:19 +0800

    correct pwd fetch

--- cmd/sign.go
+++ cmd/sign.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"crypto/ecdsa"
 	"fmt"
+	"io/ioutil"
 	"math/big"
 	"path/filepath"
 	"strings"
@@ -35,7 +36,7 @@ func signRawTx(cmd *cobra.Command, _ []string) {
 	key, _ := cmd.Flags().GetString("key")
 	passwordFile, _ := cmd.Flags().GetString("password")
 
-	password, err := ReadFileToString(passwordFile)
+	password, err := fethchKeyFromFile(passwordFile)
 	if err != nil {
 		fmt.Printf("read password file error: %v", err)
 		return
@@ -48,6 +49,19 @@ func signRawTx(cmd *cobra.Command, _ []string) {
 	}
 
 	innerSignRawTx(chainId, file, privateKey)
+}
+
+func fethchKeyFromFile(path string) (string, error) {
+	text, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	lines := strings.Split(string(text), "\n")
+	// Sanitise DOS line endings.
+	for i := range lines {
+		lines[i] = strings.TrimRight(lines[i], "\r")
+	}
+	return lines[0], nil
 }
 
 func innerSignRawTx(chainId int64, file string, privateKey *ecdsa.PrivateKey) {
